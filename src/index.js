a6d10b9b6d9eb41a2182d58f6e4a21e3095cc07f
NicoB-Code <82388053+NicoB-Code@users.noreply.github.com>
2022-11-12 13:53:44 +0100

    CollisionDetection Working
    Example of Collision Detection now fully works

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import "./style.css";
 import * as THREE from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
-import { Vector3 } from "three";
+import { Box3, Vector3 } from "three";
 
 // Listener to catch keyboard inputs
 document.addEventListener("keydown", keyPressed, false);
@@ -10,12 +10,16 @@ document.addEventListener("keydown", keyPressed, false);
 let mixer; // animation mixer
 let clips; // used to store our animations
 const clock = new THREE.Clock() // timer for animations
-let xSpeed = 0.01; // base speed of the carriage in the scene
+let xSpeed = 0; // base speed of the carriage in the scene
 const scene = new THREE.Scene();
 let carriage;
+let carriageBox;
+let tree2;
+let tree2Box;
 let roadArray = new Array();
-let currentlastRoad = 0;    // determine which road needs to be set forward
+let currentLastRoad = 0;    // determine which road needs to be set forward
 let roadCounter = 0;
+
 
 // our point of view, the camera
 const camera = new THREE.PerspectiveCamera(50,window.innerWidth / window.innerHeight,1,1000);
@@ -31,35 +35,34 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.render(scene, camera);
 
 function loadRoad(){
+    let arrayLength;
     gltfLoader.load(
         "objects/gravel_road/blenderRoad.glb",
         function(glb) {
-            for(let i=0; i<100; i++){
-                roadArray.unshift(glb.scene.clone().children[0]);
-                roadArray[0].scale.set(0.1,0.1,0.1);
-                roadArray[0].position.set(0.538*i,0.017,-0.23);
-                roadArray[0].rotateY(Math.PI/2);
-                roadArray[0].rotateX(-Math.PI/110);
-                scene.add(roadArray[0])
+            for(let i=0; i<40; i++){
+                arrayLength = roadArray.push(glb.scene.clone().children[0]);
+                roadArray[arrayLength-1].scale.set(0.1,0.1,0.1);
+                roadArray[arrayLength-1].position.set(0.538*i,0.017,-0.23);
+                roadArray[arrayLength-1].rotateY(Math.PI/2);
+                roadArray[arrayLength-1].rotateX(-Math.PI/110);
+                scene.add(roadArray[arrayLength-1])
             }
-            for(let i=0; i<100; i++){
-                roadArray.unshift(glb.scene.clone().children[0]);
-                roadArray[0].scale.set(0.1,0.1,0.1);
-                roadArray[0].position.set(0.538*i,0.017,0);
-                roadArray[0].rotateY(Math.PI/2);
-                roadArray[0].rotateX(-Math.PI/110);
-                scene.add(roadArray[0])
+            for(let i=0; i<40; i++){
+                arrayLength = roadArray.push(glb.scene.clone().children[0]);
+                roadArray[arrayLength-1].scale.set(0.1,0.1,0.1);
+                roadArray[arrayLength-1].position.set(0.538*i,0.017,0);
+                roadArray[arrayLength-1].rotateY(Math.PI/2);
+                roadArray[arrayLength-1].rotateX(-Math.PI/110);
+                scene.add(roadArray[arrayLength-1])
             }
-            for(let i=0; i<100; i++){
-                roadArray.unshift(glb.scene.clone().children[0]);
-                roadArray[0].scale.set(0.1,0.1,0.1);
-                roadArray[0].position.set(0.538*i,0.017,0.23);
-                roadArray[0].rotateY(Math.PI/2);
-                roadArray[0].rotateX(-Math.PI/110);
-                scene.add(roadArray[0])
+            for(let i=0; i<40; i++){
+                arrayLength = roadArray.push(glb.scene.clone().children[0]);
+                roadArray[arrayLength-1].scale.set(0.1,0.1,0.1);
+                roadArray[arrayLength-1].position.set(0.538*i,0.017,0.23);
+                roadArray[arrayLength-1].rotateY(Math.PI/2);
+                roadArray[arrayLength-1].rotateX(-Math.PI/110);
+                scene.add(roadArray[arrayLength-1])
             }
- 
-
         });
 }
 
@@ -70,16 +73,38 @@ const loadCarriage = () => {
             carriage = glb.scene.children[0];
             carriage.scale.set(0.1, 0.1, 0.1);
             carriage.position.set(0,0.085,0);
-            scene.add(glb.scene);
+            carriage.rotateZ(Math.PI)
+            scene.add(carriage);
             mixer = new THREE.AnimationMixer(glb.scene); // create animation mixer for current object
             clips = glb.animations;   // all of our clips
+            carriageBox = new Box3().setFromObject(carriage);
+            const helper = new THREE.Box3Helper(carriageBox,0xffff00);
+            scene.add(helper)
+
+/*             const size = box.getSize(new Vector3()).length();
+            const center = box.getCenter(new Vector3()); */
+        }
+    );
+};
+
+const loadTree2 = () => {
+    gltfLoader.load(
+        "objects/obstacles/baum2.glb",
+        (glb) => {
+            tree2 = glb.scene.children[0];
+            tree2.scale.set(0.05,0.05,0.05)
+            tree2.position.set(2,0.05,0);
+            scene.add(tree2);
+            tree2Box = new Box3().setFromObject(tree2);
+
+/*             const size = box.getSize(new Vector3()).length();
+            const center = box.getCenter(new Vector3()); */
         }
     );
 };
 
 const loadingManager = new THREE.LoadingManager();
 const gltfLoader = new GLTFLoader(loadingManager);
-//const controls = new OrbitControls(camera, renderer.domElement);
 
 loadingManager.onLoad = () => {
     camera.updateProjectionMatrix();
@@ -110,12 +135,11 @@ async function setupScene(){
         "./Skyboxes/Textures/SkyMidNight_Back.png",  // 6, negZ
     ]);
     scene.background = texture;
-    const grid = new THREE.GridHelper(30,300);
-    //scene.add(grid);
     // We need to load our objects
     loadCarriage();
     loadRoad();
-    console.log(roadArray)
+    loadTree2();
+
 };
 
 const animate = () => {
@@ -128,35 +152,55 @@ const animate = () => {
     if(mixer) // dont try to update animations, if they are not created yet
         mixer.update(clock.getDelta()) 
     // get delta is our pointer inside the animation, in other words: What Frame is currently showing?
-    updateCarriage()    // make sure our carriage is updated
-    //if(roadCounter % 80 == 0){updateRoads();}
+    updateCarriage();    // make sure our carriage is updated
+    updateRoads();      // make sure roads that are not rendered get moved to the front
     updateCamera(oldCarriagePosition)
     renderer.render(scene, camera); // render the updated scene
 };
 
-
+/**
+ * This function moves the roads that are furthest behind the carriage and
+ * moves them to the "end of the road", so that the street is technically
+ * endless
+ */
 function updateRoads(){
-    console.log(roadArray)
-    console.log(currentlastRoad)
-    // index 0 to 4 middle Road
-    roadArray[currentlastRoad].position.x += (4*0.538)
-    // index 4 to  left Road
-    roadArray[currentlastRoad+4].position.x += (4*0.538)
-    // index 10 to 14 right road
-    roadArray[currentlastRoad+8].position.x += (4*0.538)
-    if(currentlastRoad == 3){
-        currentlastRoad = 0;
-    } else {
-        currentlastRoad++;
-    }
+    /*  carriage starting position is: (0,0.085,0)
+    because the carriage only moves in the x-direction it is the only thing 
+    we need to evaluate */
+    let carriageXPos  = carriage.position.x;
+    let currentLastRoadXPos = roadArray[currentLastRoad].position.x;
+    let diffToCarriage = carriageXPos - currentLastRoadXPos;
 
+    /* we have three tracks - their indices are as follows 
+    track 1: 0-29
+    track 2: 30-59
+    track 3: 60-89
+    0.538 is the x-length of a single road segment, therefore we need to jump
+    20*x-length 
+    If the last Road is out of the rendered view (More than 3 Road segments away), move it to the front 
+    */
+    if(diffToCarriage > 1){
+        roadArray[currentLastRoad].position.x += 0.538*40;
+        roadArray[currentLastRoad+40].position.x += 0.538*40;
+        roadArray[currentLastRoad+80].position.x += 0.538*40;
+        currentLastRoad++;
+    }
+    if(currentLastRoad > 39){
+        currentLastRoad = 0;
+    }
+    
 }
 /**
  * This function is used to update animations and position of the carriage accordingly
  * It checks for the x-axis speed of the carriage and evaluates it.
  */
 function updateCarriage(){
-    carriage.position.x += xSpeed
+    carriageBox = new Box3().setFromObject(carriage);
+    let doesCollide = carriageBox.intersectsBox(tree2Box);
+    console.log(doesCollide);
+
+    carriage.position.x += xSpeed;
+    carriageBox.x += xSpeed;
     if (xSpeed > 0){
         clips.forEach(function(clip) {
             const action = mixer.clipAction(clip);
@@ -203,9 +247,12 @@ function keyPressed(event){
             break;
         case 65:
             xSpeed += 0.01;
+            console.log(xSpeed);
             break;
         case 68:
             xSpeed -= 0.01;
+            console.log(xSpeed);
+
             break;
     }
 }
