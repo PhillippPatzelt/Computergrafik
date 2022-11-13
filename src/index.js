65d701f70b69b4cd58fd06a48325aabbc6c08e9f
NicoB-Code <82388053+NicoB-Code@users.noreply.github.com>
2022-11-13 23:52:49 +0100

    first fully functional prototype
    Walls need to be moved when out of rendered view, but everything else works now.

--- src/index.js
+++ src/index.js
@@ -1,28 +1,46 @@
 import "./style.css";
 import * as THREE from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+//import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import { Box3, Vector3 } from "three";
+
 
 // Listener to catch keyboard inputs
 document.addEventListener("keydown", keyPressed, false);
 
 let mixer; // animation mixer
+let highscore = 0; // high-score for the player
+let gamestarted = false;
 let clips; // used to store our animations
-const clock = new THREE.Clock() // timer for animations
-let xSpeed = 0; // base speed of the carriage in the scene
+const clock = new THREE.Clock(); // timer for animations
+let xSpeed; // base speed of the carriage in the scene
 const scene = new THREE.Scene();
+let hasLoaded = false;
 let carriage;
 let carriageBox;
-let tree2;
-let tree2Box;
+let wallArray = new Array();
+let wallBoxArray = new Array();
+let tree2Array = new Array();
+let tree2BoxArray =  new Array();
 let roadArray = new Array();
+let isFirstTime = true;
 let currentLastRoad = 0;    // determine which road needs to be set forward
-let roadCounter = 0;
+let currentLastTree2 = 0;
+let roadPositionsZ = [-0.23, 0, 0.23];  // left road, middle road, right road
+const highscoreElement = document.getElementById("highscore");
+const gameoverElement = document.getElementById("gameover");
+const canvasElement = document.querySelector(".canv");
+gameoverElement.style.visibility='hidden';
+highscoreElement.style.visibility='hidden';
+canvasElement.style.visibility="hidden";
 
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
 
 // our point of view, the camera
-const camera = new THREE.PerspectiveCamera(50,window.innerWidth / window.innerHeight,1,1000);
+let camAspect = window.innerWidth / window.innerHeight
+const camera = new THREE.PerspectiveCamera(50,camAspect,0.05,2000);
 
 // used to draw our objects
 const renderer = new THREE.WebGLRenderer({
@@ -66,6 +84,33 @@ function loadRoad(){
         });
 }
 
+function loadWalls(){
+    let arrayLength;
+    gltfLoader.load(
+        "objects/Wall/stone_wall_nr2.glb",
+        (gltf) => {
+            // left side
+            for(let i=0; i<5; i++){
+                arrayLength = wallArray.push(gltf.scene.clone().children[0]);
+                wallArray[arrayLength-1].scale.set(0.7,0.7,0.7);
+                wallArray[arrayLength-1].position.set(15*i,1.15,2.4);
+                scene.add(wallArray[arrayLength-1])
+                wallBoxArray.push(new Box3().setFromObject(wallArray[arrayLength-1]));
+            }
+            // right side
+            for(let i=0; i<5; i++){
+                arrayLength = wallArray.push(gltf.scene.clone().children[0]);
+                wallArray[arrayLength-1].scale.set(0.7,0.7,0.7);
+                wallArray[arrayLength-1].position.set(14*i,1.15,-2.4);
+                wallArray[arrayLength-1].rotateZ(Math.PI+Math.PI/65)
+                wallArray[arrayLength-1].rotateX(-Math.PI/6.3)
+                wallArray[arrayLength-1].rotateY(Math.PI/28)
+                scene.add(wallArray[arrayLength-1])
+                wallBoxArray.push(new Box3().setFromObject(wallArray[arrayLength-1]));
+            }
+        });
+}
+
 const loadCarriage = () => {
     gltfLoader.load(
         "objects/animierteKutsche.glb",
@@ -75,11 +120,10 @@ const loadCarriage = () => {
             carriage.position.set(0,0.085,0);
             carriage.rotateZ(Math.PI)
             scene.add(carriage);
-            mixer = new THREE.AnimationMixer(glb.scene); // create animation mixer for current object
+            mixer = new THREE.AnimationMixer(carriage); // create animation mixer for current object
             clips = glb.animations;   // all of our clips
             carriageBox = new Box3().setFromObject(carriage);
-            const helper = new THREE.Box3Helper(carriageBox,0xffff00);
-            scene.add(helper)
+
 
 /*             const size = box.getSize(new Vector3()).length();
             const center = box.getCenter(new Vector3()); */
@@ -87,18 +131,23 @@ const loadCarriage = () => {
     );
 };
 
-const loadTree2 = () => {
+const loadTrees2 = () => {
+    let arrayLength;
     gltfLoader.load(
         "objects/obstacles/baum2.glb",
         (glb) => {
-            tree2 = glb.scene.children[0];
-            tree2.scale.set(0.05,0.05,0.05)
-            tree2.position.set(2,0.05,0);
-            scene.add(tree2);
-            tree2Box = new Box3().setFromObject(tree2);
-
-/*             const size = box.getSize(new Vector3()).length();
-            const center = box.getCenter(new Vector3()); */
+            for(let i=0; i<20; i++){
+                /* z-position   left side: -0.23 
+                                middle: 0
+                                right side: 0.23
+                */
+                arrayLength = tree2Array.push(glb.scene.clone().children[0]);
+                tree2Array[arrayLength-1].scale.set(0.05,0.05,0.05);
+                tree2Array[arrayLength-1].position.set(2*i+5,0.05,roadPositionsZ[getRandomInt(3)]);
+                scene.add(tree2Array[arrayLength-1]);
+                tree2BoxArray.push(new Box3().setFromObject(tree2Array[arrayLength-1]));
+                tree2BoxArray[arrayLength-1].max.z -= 0.05
+            }
         }
     );
 };
@@ -109,7 +158,8 @@ const gltfLoader = new GLTFLoader(loadingManager);
 loadingManager.onLoad = () => {
     camera.updateProjectionMatrix();
     camera.translateZ(-0.2);
-    animate();
+    animate()
+    hasLoaded = true;
 };
 
 loadingManager.onProgress = (url, itemsLoaded, itemsTotal) => {
@@ -121,10 +171,10 @@ loadingManager.onProgress = (url, itemsLoaded, itemsTotal) => {
  */
 async function setupScene(){
     // add lighting to our scene
-    const ambientlight = new THREE.AmbientLight(0xffffff, 3);
-    scene.add(ambientlight);
+    const directionalLight = new THREE.DirectionalLight( 0xffffff, 1 );
+    scene.add(directionalLight);
 
-    const texLoader = new THREE.CubeTextureLoader();
+    /* const texLoader = new THREE.CubeTextureLoader();
     // need to alter Skybox
     const texture = texLoader.load([
         "./Skyboxes/Textures/SkyMidNight_Right.png", // 1, posX
@@ -134,30 +184,57 @@ async function setupScene(){
         "./Skyboxes/Textures/SkyMidNight_Front.png", // 5, posZ
         "./Skyboxes/Textures/SkyMidNight_Back.png",  // 6, negZ
     ]);
-    scene.background = texture;
+    scene.background = texture; */
     // We need to load our objects
     loadCarriage();
     loadRoad();
-    loadTree2();
-
+    loadTrees2();
+    loadWalls();
 };
 
 const animate = () => {
-    roadCounter++;
     // store the position of our carriage for updating camera
     const oldCarriagePosition = new Vector3();
     carriage.getWorldPosition(oldCarriagePosition);
     requestAnimationFrame(animate);
     // controls.update(); // used to focus camera on center 
     if(mixer) // dont try to update animations, if they are not created yet
-        mixer.update(clock.getDelta()) 
+        mixer.update(clock.getDelta())
     // get delta is our pointer inside the animation, in other words: What Frame is currently showing?
-    updateCarriage();    // make sure our carriage is updated
-    updateRoads();      // make sure roads that are not rendered get moved to the front
-    updateCamera(oldCarriagePosition)
+    if(gamestarted){
+        //for(let j=0; j<20; j++){wallArray[6].rotateY(-Math.PI/2512) } - testing purpose
+        updateCarriage();    // make sure our carriage is updated
+        updateRoads();      // make sure roads that are not rendered get moved to the front
+        updateTrees2();
+        updateCamera(oldCarriagePosition)
+        highscore += 0.5;
+        highscoreElement.innerHTML = new String(highscore)
+    }
     renderer.render(scene, camera); // render the updated scene
 };
 
+function updateTrees2(){
+        /*  carriage starting position is: (0,0.085,0)
+    because the carriage only moves in the x-direction it is the only thing 
+    we need to evaluate */
+    let carriageXPos  = carriage.position.x;
+    let currentLastTreeXPos = tree2Array[currentLastTree2].position.x;
+    let diffToCarriage = carriageXPos - currentLastTreeXPos;
+    /*
+    0.538 is the x-length of a single road segment, therefore we need to jump
+    40*x-length 
+    If the last Tree is out of the rendered view (More than 1 Road segments away), move it to the front 
+    */
+    if(diffToCarriage > 1){
+        tree2Array[currentLastTree2].position.x += 0.538*60;
+        tree2BoxArray[currentLastTree2].max.x += 0.538*60;
+        tree2BoxArray[currentLastTree2].min.x += 0.538*60;
+        currentLastTree2++;
+    }
+    if(currentLastTree2 > 19){
+        currentLastTree2 = 0;
+    }
+}
 /**
  * This function moves the roads that are furthest behind the carriage and
  * moves them to the "end of the road", so that the street is technically
@@ -177,7 +254,7 @@ function updateRoads(){
     track 3: 60-89
     0.538 is the x-length of a single road segment, therefore we need to jump
     20*x-length 
-    If the last Road is out of the rendered view (More than 3 Road segments away), move it to the front 
+    If the last Road is out of the rendered view (More than 1 Road segments away), move it to the front 
     */
     if(diffToCarriage > 1){
         roadArray[currentLastRoad].position.x += 0.538*40;
@@ -190,35 +267,47 @@ function updateRoads(){
     }
     
 }
+
 /**
  * This function is used to update animations and position of the carriage accordingly
  * It checks for the x-axis speed of the carriage and evaluates it.
  */
 function updateCarriage(){
     carriageBox = new Box3().setFromObject(carriage);
-    let doesCollide = carriageBox.intersectsBox(tree2Box);
-    console.log(doesCollide);
-
+    let doesCollide = carriageBox.intersectsBox(tree2BoxArray[currentLastTree2]);
+    console.log(carriageBox,tree2BoxArray[currentLastTree2])
+    console.log(xSpeed)
+    if(doesCollide){
+        gameOver();
+    }  
     carriage.position.x += xSpeed;
-    carriageBox.x += xSpeed;
     if (xSpeed > 0){
         clips.forEach(function(clip) {
             const action = mixer.clipAction(clip);
             action.play()
-            action.timeScale = (-xSpeed)*100;
+            action.timeScale = (xSpeed)*100;
         })
-    }   else if( xSpeed < 0){
-        clips.forEach(function(clip) {
-            const action = mixer.clipAction(clip);
-            action.play()
-            action.timeScale = (-xSpeed)*100;
-        })
-    }else {
+    } else {
         clips.forEach(function(clip) {
             const action = mixer.clipAction(clip);
             action.stop()
         })
     }
+}
+
+function gameOver(){
+    xSpeed = 0; // if game is over, stop the carriage
+    gamestarted = false;
+    highscoreElement.style.visibility='hidden';
+    gameoverElement.style.visibility='visible';
+    gameoverElement.innerHTML = "GAME OVER<br>PRESS ESC TO TRY AGAIN<br>YOUR HIGHSCORE WAS:" + String(highscore)
+} 
+
+/**
+ * If the game gets restarted, all of the objects need to be reset
+ */
+function rearrangeObjects(){
+
 }
 
 /**
@@ -232,27 +321,36 @@ function updateCamera(oldPosition){
     const delta = newCarriagePosition.clone().sub(oldPosition); // difference between the two points
     let difDelta = new Vector3(xSpeed,0,0)
     camera.position.add(difDelta) // add difference, to move to object 
-
 }
 
 function keyPressed(event){
     let keyNumber = event.which
     switch(keyNumber){
-        case 27: 
-            carriage.position.set(0,0.085,0);
-            camera.position.set(-1.25,1,0);
-            camera.rotateY(-Math.PI/2)
-            camera.rotateX(-Math.PI/7)
-            xSpeed = 0;
+        case 27:
+            if(!gamestarted && hasLoaded){
+                gameoverElement.style.visibility="hidden";
+                canvasElement.style.visibility="visible"; // make canvas visible so that background is not visible anymore
+                carriage.position.set(0,0.085,0);
+                camera.position.set(-1.25,1,0);
+                if(isFirstTime){
+                    camera.rotateY(-Math.PI/2);
+                    camera.rotateX(-Math.PI/6.2);
+                    isFirstTime = false;
+                } else{
+                    rearrangeObjects();
+                }
+                xSpeed = 0.02;
+                gamestarted = true;
+                highscore = 0;
+                highscoreElement.style.visibility='visible';
+
+            } 
             break;
         case 65:
-            xSpeed += 0.01;
-            console.log(xSpeed);
+            carriage.position.z -= 0.23;
             break;
         case 68:
-            xSpeed -= 0.01;
-            console.log(xSpeed);
-
+            carriage.position.z += 0.23;
             break;
     }
 }
